e63fdeedfc7a2ea77a05fcea5b3e1f756bb35699
alaskai <164151425+ST10444972-Ethan-Algeo@users.noreply.github.com>
2025-08-28 22:50:09 +0200

    Reorganized Repo
    Repo fixed

--- CLDV6212-ST10444972-POE/CLDV6212-ST10444972-POE/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CLDV6212_ST10444972_POE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
